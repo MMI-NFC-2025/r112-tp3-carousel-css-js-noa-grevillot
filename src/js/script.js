73bb11f35a9361c6115ebc36ad616fd4a569eaef
noa-grevillot <noa.grevillot@edu.univ-fcomte.fr>
2025-11-19 14:50:30 +0100

    defilement suivant/précédent en javascript

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,22 @@
 // *** CAROUSEL ***
-// TODO
-carousel = document.querySelector("carousel__container");
+// TODO 
+
+
+const carousel = document.querySelector(".carousel__container");
+const prevButton = document.querySelector(".carousel__button--prev");
+const nextButton = document.querySelector(".carousel__button--next");
+
+const premierItem = document.querySelector(".carousel__item");
+const scrollAmount = premierItem.clientWidth;
+
+if (carousel) {
+  // Scroll au clic sur le bouton précédent
+  prevButton.addEventListener("click", () => {
+    carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+  });
+
+  // Scroll au clic sur le bouton suivant
+  nextButton.addEventListener("click", () => {
+    carousel.scrollBy({ left: scrollAmount, behavior: "smooth" });
+  });
+}
